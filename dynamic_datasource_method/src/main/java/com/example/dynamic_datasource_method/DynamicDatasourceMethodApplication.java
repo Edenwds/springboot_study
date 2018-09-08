24176193b84ff5494e4eed43ba796e14a0e7e4aa
wds <1466809510@qq.com>
2018-09-08 10:34:39 +0800

    Update DynamicDatasourceMethodApplication.java

--- dynamic_datasource_method/src/main/java/com/example/dynamic_datasource_method/DynamicDatasourceMethodApplication.java
+++ dynamic_datasource_method/src/main/java/com/example/dynamic_datasource_method/DynamicDatasourceMethodApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-//@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class DynamicDatasourceMethodApplication {
 
